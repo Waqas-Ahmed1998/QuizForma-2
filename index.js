06e7ec70691dff30e62a4db7ec531f85ccc27c22
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-05-18 00:44:06 +0500

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// $(document).ready(function () {
-//   $("#btn-login").click(function () {
-//     $("#background").show();
-//     $("#popup-wrapper").show();
-//   });
-//   $("#cross").click(function () {
-//     $("#background").hide();
-//     $("#popup-wrapper").hide();
-//   });
-//   $("#background").click(function () {
-//     $(this).hide();
+$(document).ready(function () {
+  // popup-1
+  $("#btn-1-next").click(function () {
+    $("#popup-2").show();
+    $("#popup-1").hide();
+  });
 
-//     $("#popup-wrapper").hide();
-//   });
-// });
+  $("#btn-1-prev").click(function () {
+    $("#popup-1").show();
+    $("#popup-2").hide();
+  });
+
+  $("#btn-2-prev").click(function () {
+    $("#popup-1").show();
+    $("#popup-2").hide();
+  });
+});
