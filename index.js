203d3c476522266001ad71b88afd31a05eb72210
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2022-05-19 20:16:01 +0500

    added rest stapes

--- index.js
+++ index.js
@@ -5,13 +5,70 @@ $(document).ready(function () {
     $("#popup-1").hide();
   });
 
-  $("#btn-1-prev").click(function () {
-    $("#popup-1").show();
-    $("#popup-2").hide();
-  });
+  // btn number shows the popup number
 
   $("#btn-2-prev").click(function () {
     $("#popup-1").show();
     $("#popup-2").hide();
+  });
+
+  $("#btn-2-next").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").show();
+  });
+
+  $("#btn-3-prev").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").show();
+    $("#popup-3").hide();
+  });
+
+  $("#btn-3-next").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").hide();
+    $("#popup-4").show();
+  });
+
+  $("#btn-4-prev").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").show();
+    $("#popup-4").hide();
+  });
+
+  $("#btn-4-next").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").hide();
+    $("#popup-4").hide();
+    $("#popup-5").show();
+  });
+
+  $("#btn-5-prev").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").hide();
+    $("#popup-4").show();
+    $("#popup-5").hide();
+  });
+
+  $("#btn-5-next").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").hide();
+    $("#popup-4").hide();
+    $("#popup-5").hide();
+    $("#popup-6").show();
+  });
+
+  $("#btn-6-prev").click(function () {
+    $("#popup-1").hide();
+    $("#popup-2").hide();
+    $("#popup-3").hide();
+    $("#popup-4").hide();
+    $("#popup-5").show();
+    $("#popup-6").hide();
   });
 });
